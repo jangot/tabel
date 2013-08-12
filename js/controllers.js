65905bcdd49a45feb0507807362abb35863e9c80
jangot <pulin.pavel@gmail.com>
2013-08-12 14:37:08 +0400

    load

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,17 @@ function TableCtrl($scope, $http, $filter) {
     $scope.head = {};
     $scope.body = [];
     $scope.currentItems = [];
+    $scope.query = ''
+    $scope.filterItems = [];
     $scope.currentPosition = 0;
     $scope.maxItems = 50;
     $scope.pages = [];
+    $scope.selectedType = 'small';
+    $scope.types = {
+        small : 'Small',
+        large : 'Large',
+        their : 'Their'
+    }
 
     $scope.prev = function() {
         if ($scope.currentPosition > 0) {
@@ -23,30 +31,55 @@ function TableCtrl($scope, $http, $filter) {
         }
     }
     $scope.setPosition = function(position) {
-        var maxPosition = $scope.body.length / $scope.maxItems;
+        var maxPosition = $scope.filterItems.length / $scope.maxItems;
         if (position > maxPosition || position < 0) {
             return;
         }
         $scope.currentPosition = position;
-
+        updateCurrentItems();
     };
 
     $scope.search = function() {
+        if ($scope.query) {
+            $scope.filterItems = filterByString($scope.body, $scope.query);
+            $scope.currentPosition = 0;
+        } else {
+            $scope.filterItems = $scope.body;
+        }
         updateCurrentItems();
     }
-    $http.get('http://thethz.com/dataset.php?type=large').success(function(data) {
+
+    $scope.load = function(type) {
+        if (!$scope.types[type]) {
+            return;
+        }
+        $scope.selectedType = type;
+        if (type == 'their') {
+            loadFile(function(data) {
+                setData(data);
+                $scope.$apply()
+            });
+        } else {
+            $http.get('http://thethz.com/dataset.php?type=' + type).success(function(data) {
+                setData(data);
+            });
+        }
+    }
+
+
+
+     function setData(data) {
         $scope.head = data.shift();
         $scope.body = data;
+        $scope.filterItems = data;
+        $scope.currentPosition = 0;
+        $scope.query = '';
         updateCurrentItems();
-    });
+    }
 
     function updateCurrentItems() {
         var newCurrentItems = [];
-        var items = $scope.body;
-
-        if ($scope.query) {
-            items = filterByString(items, $scope.query);
-        }
+        var items = $scope.filterItems;
 
         var start = $scope.maxItems * $scope.currentPosition;
         var finish = start + $scope.maxItems;
@@ -57,8 +90,7 @@ function TableCtrl($scope, $http, $filter) {
             newCurrentItems.push(items[i]);
         }
         $scope.currentItems = newCurrentItems;
-
-        $scope.pages.length = Math.ceil($scope.body.length / $scope.maxItems);
+        $scope.pages.length = Math.ceil(items.length / $scope.maxItems);
     }
 
     function filterByString(items, query) {
@@ -72,4 +104,20 @@ function TableCtrl($scope, $http, $filter) {
             return false;
         });
     };
+
+    function loadFile(cb) {
+        var r = new FileReader();
+        r.onload = function (oFREvent) {
+            var text = oFREvent.target.result;
+            cb(JSON.parse(text))
+        };
+
+        var fileSelector = document.createElement('input');
+        fileSelector.setAttribute('type', 'file');
+        fileSelector.onchange = function(e) {
+            var file = e.target.files[0];
+            r.readAsText(file);
+        }
+        fileSelector.click();
+    }
 }
